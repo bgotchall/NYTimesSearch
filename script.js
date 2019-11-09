07396f77ece3740165b1edf43b85316a89b6b3d2
austinev <bgotchall@gmail.com>
2019-11-09 11:54:14 -0600

    fixing index bug.  basically works now

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // example call: https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yourkey
 //<script type="text/javascript">
 var my_key= "KVtPDlU5TfAMaVQLWvsYQQo6sta3L8Dz";
-var question= "Iraq"
+var question= $("#searchTerm").val();
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + question + "&api-key="+ my_key;
 var num_responses=5;
 
@@ -11,7 +11,7 @@ var num_responses=5;
 
 
 function get_a_result(){
-    $("#target_div").empty();
+    $("#articles").empty();
     console.log(queryURL);
     $.ajax({
         url: queryURL,
@@ -28,13 +28,13 @@ function get_a_result(){
             console.log(response.response.docs[i].byline.original);
             console.log(response.response.docs[i].web_url);
             var_new_div=$("<div>");
-            $(var_new_div).append("<H3>"+response.response.docs[0].headline.main+"</H3>")
-            $(var_new_div).append("<p>"+response.response.docs[0].abstract+"</p>")
-            $(var_new_div).append("<p>"+response.response.docs[0].pub_date+"</p>")
-            $(var_new_div).append("<p>"+response.response.docs[0].byline.original+"</p>")
-            console.log("<a href=\""+response.response.docs[0].web_url+"\">"+response.response.docs[0].web_url+"</a>");
-            $(var_new_div).append("<a href=\""+response.response.docs[0].web_url+"\">"+response.response.docs[0].web_url+"</a>")
-            $("#target_div").append(var_new_div);
+            $(var_new_div).append("<H3>"+response.response.docs[i].headline.main+"</H3>")
+            $(var_new_div).append("<p>"+response.response.docs[i].abstract+"</p>")
+            $(var_new_div).append("<p>"+response.response.docs[i].pub_date+"</p>")
+            $(var_new_div).append("<p>"+response.response.docs[i].byline.original+"</p>")
+            console.log("<a href=\""+response.response.docs[i].web_url+"\">"+response.response.docs[i].web_url+"</a>");
+            $(var_new_div).append("<a href=\""+response.response.docs[i].web_url+"\">"+response.response.docs[i].web_url+"</a>")
+            $("#articles").append(var_new_div);
         }
         
        
