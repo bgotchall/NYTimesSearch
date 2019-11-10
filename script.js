7ce4e09709d943b325fbc51c2ab2359bf65d30ce
austinev <bgotchall@gmail.com>
2019-11-09 21:57:40 -0600

    added support for dates.  switched the font color in the hover definitions in style.css

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 //api key: KVtPDlU5TfAMaVQLWvsYQQo6sta3L8Dz
 // api secret: zwIH5Nd32sRsGhns
 // example call: https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yourkey
-//<script type="text/javascript">
+
 var my_key= "KVtPDlU5TfAMaVQLWvsYQQo6sta3L8Dz";
 var question= $("#searchTerm").val();
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + question + "&api-key="+ my_key;
-var num_responses=5;
-
-
+var queryURL;
+var num_responses=$("#numRecords").val();
 
 
 function get_a_result(){
+    //here the query is built, send it, wait for a response and fill up Div's with the responses, up to the requested number.
     $("#articles").empty();
-    console.log(queryURL);
+    //console.log(queryURL);
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -21,31 +20,61 @@ function get_a_result(){
 
         for (var i=0; i<num_responses; i++){
             console.log(response);
-            console.log(response.response.docs[i]);
-            console.log(response.response.docs[i].headline.main);
-            console.log(response.response.docs[i].abstract);
-            console.log(response.response.docs[i].pub_date);
-            console.log(response.response.docs[i].byline.original);
-            console.log(response.response.docs[i].web_url);
+          //  console.log(response.response.docs[i]);
+            
             var_new_div=$("<div>");
             $(var_new_div).append("<H3>"+response.response.docs[i].headline.main+"</H3>")
             $(var_new_div).append("<p>"+response.response.docs[i].abstract+"</p>")
             $(var_new_div).append("<p>"+response.response.docs[i].pub_date+"</p>")
             $(var_new_div).append("<p>"+response.response.docs[i].byline.original+"</p>")
-            console.log("<a href=\""+response.response.docs[i].web_url+"\">"+response.response.docs[i].web_url+"</a>");
+           // console.log("<a href=\""+response.response.docs[i].web_url+"\">"+response.response.docs[i].web_url+"</a>");
             $(var_new_div).append("<a href=\""+response.response.docs[i].web_url+"\">"+response.response.docs[i].web_url+"</a>")
             $("#articles").append(var_new_div);
         }
         
-       
-
-        
-       
-
 
     });
 }
 
-get_a_result();
 
-//</script>
+$("#searchBtn").on("click",function(event){
+    //build the search URL and send it.
+    event.preventDefault();
+    
+     question= $("#searchTerm").val();
+     var temp_string=buildDateString();
+     queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + question +buildDateString()+ "&api-key="+ my_key;
+     
+     console.log(queryURL);
+     num_responses=$("#numRecords").val();
+     get_a_result();
+
+});
+
+function buildDateString(){
+//build the filter portion of the URL by searching the optional date inputs.  If either are blank,return blank.
+    var start_year = parseInt($("#startYear").val());
+    var end_year = parseInt($("#endYear").val());
+    console.log ("start year: "+start_year);
+    console.log ("end year: "+ end_year);
+
+    //"&fq=pub_year:("+temp_string+")
+    var temp_str="";
+
+    if ((start_year>0)&&(end_year>0)){
+        //optional years ahve been added.
+        for (var i=parseInt(start_year); i<parseInt(end_year+1);i++){
+            temp_str=temp_str+'"' +i+'"' ;         //add the year and a space
+        }
+        console.log("date string is: "+temp_str);
+        var answer='&fq=pub_year:('+temp_str+')';
+        console.log("total filter is: "+answer);
+        return answer;
+    } else {
+       //no years requested.  the filter part should be blank
+        return "";
+    }
+
+    return temp_str;
+
+};
